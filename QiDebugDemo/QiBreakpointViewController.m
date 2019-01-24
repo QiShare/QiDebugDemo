88a3f1e687b601254a12ef30bf92657c3ec45992
QiShare <1928386499@qq.com>
2019-01-24 16:58:23 +0800

    breakpoints1

--- QiDebugDemo/QiBreakpointViewController.m
+++ QiDebugDemo/QiBreakpointViewController.m
@@ -15,18 +15,20 @@
 @implementation QiBreakpointViewController
 
 - (void)viewDidLoad {
+    
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (IBAction)testBreakpoints:(id)sender {
+    
+    [self testAllExceptions];
 }
-*/
+
+//! 测试全局断点
+- (void)testAllExceptions {
+ 
+    NSArray *array = @[@0, @1, @2, @3];
+    NSLog(@"The 5th value is %@", array[4]);
+}
 
 @end
